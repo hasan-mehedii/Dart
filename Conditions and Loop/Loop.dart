4fe00e3ec45b8763122413bfc5044a3a2369ea73
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-14 01:32:39 +0600

    Create Loop.dart

---
+++ Conditions and Loop/Loop.dart
@@ -0,0 +1,26 @@
+// For each Loop
+// The for each loop iterates over all list elements or variables. It is useful when you want to loop through list/collection. 
+// void main(){
+//   List<String> footballplayers=['Ronaldo','Messi','Neymar','Hazard'];
+//   footballplayers.forEach( (names)=>print(names));
+// }
+
+void main() {
+
+  // For Loop
+  for (int i = 0; i < 10; i++) {
+    print("John Doe");
+  }
+
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
+
+  int i = 1;
+  do {
+    print(i);
+    i++;
+  } while (i <= 10);
+}
+
